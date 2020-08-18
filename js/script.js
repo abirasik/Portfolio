5004d46bb64b2e1e6ff9222638faf1d78bd52545
abirasik <abirasik@gmail.com>
2020-08-19 02:50:43 +0600

    resize

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ $(window).scroll(function () {
   $scrollamout = $(window).scrollTop();
 
   if ($scrollamout > 30) {
-      $(".menu").addClass("sticky")
+    $(".menu").addClass("sticky")
   } else {
-      $(".menu").removeClass("sticky")
+    $(".menu").removeClass("sticky")
   }
 
   if ($scrollamout > 500) {
-      $(".btop").fadeIn()
+    $(".btop").fadeIn()
   } else {
-      $(".btop").fadeOut()
+    $(".btop").fadeOut()
   }
 })
 
@@ -19,7 +19,7 @@ $(window).scroll(function () {
 
 $(".btop").click(function () {
   $("html,body").animate({
-      scrollTop: 0
+    scrollTop: 0
   }, 2000)
 })
 
@@ -31,12 +31,12 @@ $(".btop").click(function () {
 
 $("#banner-part").vegas({
   slides: [
-      { src: 'images/banner-bg1.png' },
-      { src: 'images/banner-bg2.jpg' },
-      { src: 'images/banner-bg3.png' },
-      { src: 'images/banner-bg.jpg' }
+    { src: 'images/banner-bg1.png' },
+    { src: 'images/banner-bg2.jpg' },
+    { src: 'images/banner-bg1.png' },
+    { src: 'images/banner-bg.jpg' }
   ],
-  animation: [ 'kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight' ]
+  animation: ['kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight']
 });
 
 // page animation
@@ -46,55 +46,55 @@ AOS.init();
 // typ js =================
 
 var typed = new Typed('.type', {
-    strings: [
-        'Web Designer',
-        'Freelancer',
-        
-       ],
-       typeSpeed: 60,
-       backSpeed: 60,
-       loop: true,
-  });
+  strings: [
+    'Web Designer',
+    'Freelancer',
+
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
 
 // testi-slider
 $('.testi-slider').slick({
-    dots: true,
-    infinite: true,
-    // autoplay: true,
-    speed: 1000,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+  dots: true,
+  infinite: true,
+  // autoplay: true,
+  speed: 1000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
-  // portfolio mixitup
+// portfolio mixitup
 
-  var mixer = mixitup('.port-item');
+var mixer = mixitup('.port-item');
